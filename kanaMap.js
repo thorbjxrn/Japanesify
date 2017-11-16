9b8c14b50e136d3f204cc6f5229f90cb9b67b1fd
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2017-11-16 12:49:15 +0100

    Add emoji replacement for "apple"

--- kanaMap.js
+++ kanaMap.js
@@ -1,4 +1,42 @@
 /*
-File created By Thorbjørn Bonvik, 2017
-*/
-document.body.style.border = "3px solid red";
+ * File created By Thorbjørn Bonvik, 2017.
+ * based on mozillas
+ * This file contains the Map of word --> emoji substitutions.
+ */
+
+/* exported sortedEmojiMap */
+
+let dictionary = new Map();
+dictionary.set('apple', '🍎');
+
+/*
+ * After all the dictionary entries have been set, sort them by length.
+ *
+ * Because iteration over Maps happens by insertion order, this avoids
+ * scenarios where words that are substrings of other words get substituted
+ * first, leading to the longer word's substitution never triggering.
+ *
+ * For example, the 'kya' substitution would never get triggered
+ * if the 'ya' substitution happens first because the input term 'kya'
+ * would become 'kゃ', and the search for 'kya' would not find any matches.
+ */
+let tempArray = Array.from(dictionary);
+tempArray.sort((pair1, pair2) => {
+  const firstWord = pair1[0];
+  const secondWord = pair2[0];
+
+  if (firstWord.length > secondWord.length) {
+    // The first word should come before the second word.
+    return -1;
+  }
+  if (secondWord.length > firstWord.length) {
+    // The second word should come before the first word.
+    return 1;
+  }
+
+  // The words have the same length, it doesn't matter which comes first.
+  return 0;
+});
+
+// Now that the entries are sorted, put them back into a Map.
+let sortedKanaMap = new Map(tempArray);
