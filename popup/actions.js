edd14ca1b761ea45f682c32555296f94aea41d76
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2017-11-22 14:23:49 +0100

    Add different handling of pop-up interaction

--- popup/actions.js
+++ popup/actions.js
@@ -1,6 +1,9 @@
 var active = false;
 
 var sliderInput = document.querySelector('input');
+var toggleBtn = document.querySelector('.toggleBtn');
+var webLink = document.querySelector('.website');
+
 var cookieVal = { x : '1', dictionary : 'null', enabled : 'no' };
 
 
@@ -8,23 +11,24 @@ function getActiveTab() {
   return browser.tabs.query({active: true, currentWindow: true});
 }
 
-
-document.addEventListener("click",
-  function(e) {
-    if (e.target.classList.contains("toggleBtn")) {
-        console.debug("U CLICKED THE BUTTON! :O " + sliderInput);
-        refresh();
-  }
-  else if (e.target.classList.contains("website")){
-    var chosenPage = "http://" + e.target.textContent;
-  }
+//WEBSITE LINK
+webLink.onclick = function(e) {
+  console.debug("$$$!");
 }
-);
+
+//TOGGLE BUTTON FUNCTION
+toggleBtn.onclick = function(e) {
+  console.debug("U CLICKED THE BUTTON! :O " + sliderInput.value + " kanas toggled");
+}
+
 //SLIDER HANDLER
+console.debug("WTF: " + cookieVal);
+sliderInput.value = cookieVal;
+
 sliderInput.onchange = function(e) {
   getActiveTab().then((tabs) => {
     var sliderAt = e.target.value;
-    //browser.tabs.sendMessage(tabs[0].id, {activeCharacters: sliderAt});
+    browser.tabs.sendMessage(tabs[0].id, {activeCharacters: sliderAt});
 
     cookieVal.x = sliderAt;
     console.debug("Slider at: " + cookieVal.x);
