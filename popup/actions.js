32a1de0553c4eb52f2829449c8e37c491b82682d
Thorbjørn Bonvik <thorbjrn@student.chalmers.se>
2018-01-22 16:56:23 +0100

    Add update site function to toggle button and checkboxes
    also prepare for storage of options

--- popup/actions.js
+++ popup/actions.js
@@ -1,4 +1,5 @@
 /* initialise variables */
+let appEnabled = false;
 
 var checkboxes = new Map([
   ['n', document.querySelector("input[name='activeSubsN'")],
@@ -14,31 +15,62 @@ var checkboxes = new Map([
 
 var toggleButton = document.querySelector('button');
 
+/* storage */
+/*
+let gettingItem = browser.storage.local.get("appStatus");
+gettingItem.then(onGot, onError);
+
+console.debug("DEBUGG" + gettingItem);
+browser.storage.local.set({
+    appStatus:  {enabled:true}
+});
+*/
+function onGot(item) {
+  console.log(item);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+// Browser tab communication.
+
 function getActiveTab() {
   return browser.tabs.query({active: true, currentWindow: true});
 }
 
+// settings
+
 checkboxes.forEach(function(value, key) {
   value.onchange = function(e) {
       console.debug(key);
-      updatePage();
+      browser.storage.local.set({
+        key:  {enabled:true}
+      });
+      if(appEnabled){
+        updatePage(false);
+      }
   }
 });
 
 
 toggleButton.onclick = function() {
-  getActiveTab().then((tabs) => {
-    var status = true;
-
-    browser.tabs.sendMessage(tabs[0].id, {enabled: status});
-
-    console.debug("Enabled = " + status);
-
-  });
+  updatePage(true);
 }
 
-function updatePage(){
-  console.debug("UPDATE PAGE");
+// Functions
+
+function updatePage(toggleActive){ // false for update, true for toggle
+  console.debug("UPDATE PAGE! Toggle = " + toggleActive.toString());
+  getActiveTab().then((tabs) => {
+    if(toggleActive){
+      appEnabled = toggle(appEnabled);
+    }
+    browser.tabs.sendMessage(tabs[0].id, {enabled: appEnabled});
+
+    console.debug("Enabled = " + appEnabled.toString());
+
+  });
 }
 
 function toggle(status){
