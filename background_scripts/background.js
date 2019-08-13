44275d09402a3a23072b070c63e56fe1f36cbe2d
Thorbjørn Bonvik <tbonvik@gmail.com>
2019-08-13 17:50:13 +1000

    Add save state to include ha, da & yo

--- background_scripts/background.js
+++ background_scripts/background.js
@@ -11,7 +11,8 @@
     if(status == true){
       charArray = JSON.parse(localStorage.getItem('array'));
       var char2Array = [['n', charArray[0]],['a', charArray[1]],['i', charArray[2]],
-        ['u', charArray[3]], ['o', charArray[4]], ['e', charArray[5]]]; //the format used by actions.js. Might revisit this
+        ['u', charArray[3]], ['o', charArray[4]], ['e', charArray[5]],
+        ['da', charArray[6]], ['ha', charArray[7]], ['yo', charArray[8]]]; //the format used by actions.js. Might revisit this
 
         getActiveTab().then((tabs) => {
             browser.tabs.sendMessage(tabs[0].id, {enabled: true, characters: char2Array});
@@ -24,6 +25,6 @@
 
   // update when the tab is updated
   browser.tabs.onUpdated.addListener(update);
-  
+
   // update when the tab is activated
   browser.tabs.onActivated.addListener(update);
