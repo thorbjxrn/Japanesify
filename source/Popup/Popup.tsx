a717b50dd959cc5a9b1b3ff3a1817f74036dfde2
hcortizcolon <hcortizcolon@gmail.com>
2022-05-08 08:17:08 -0400

    sends correct message to tab

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -4,10 +4,16 @@ import { browser } from 'webextension-polyfill-ts';
 const Popup: React.FC = () => {
 
   const [isEnabled, setIsEnabled] = React.useState(false)
+  const [nEnabled, setNEnabled] = React.useState(false)
 
   const enableOnClick = () => {
-    browser.tabs.sendMessage(1, {})
+    browser.tabs.sendMessage(1, {enabled: !isEnabled, n: nEnabled})
     setIsEnabled(!isEnabled)
+  }
+
+  const checkBoxOnClick = () => {
+    browser.tabs.sendMessage(1, {enabled: isEnabled, n: !nEnabled})
+    setNEnabled(!nEnabled)
   }
 
   return (
@@ -23,7 +29,7 @@ const Popup: React.FC = () => {
       </button>
       <br/>
       <label id="ん-switch">ん</label>
-      <input type="checkbox" id="ん-switch" data-testid="ん-switch"/>
+      <input type="checkbox" id="ん-switch" data-testid="ん-switch" checked={nEnabled} onChange={checkBoxOnClick}/>
     </section>
   );
 };
--- test/source/Popup/Popup.test.tsx
+++ test/source/Popup/Popup.test.tsx
@@ -5,6 +5,10 @@ import "@testing-library/jest-dom/extend-expect"
 import { browser } from 'webextension-polyfill-ts';
 
 describe('Popup Component', () => {
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
     test('loads with button text enable', async () => {
         render(<Popup/>)
 
@@ -25,7 +29,25 @@ describe('Popup Component', () => {
 
         const button = screen.getByTestId('enable-button')
         fireEvent.click(button)
-      
-        expect(browser.tabs.sendMessage).toBeCalled()
+        expect(browser.tabs.sendMessage).toBeCalledWith(1, {enabled: true, n: false})
+        
+        jest.resetAllMocks()
+
+        fireEvent.click(button)
+        expect(browser.tabs.sendMessage).toBeCalledWith(1, {enabled: false, n: false})
+    })
+    
+    test('when ん checkbox is pressed it sends correct message to tab', async () => {
+        render(<Popup/>)
+
+        const checkbox = screen.getByTestId('ん-switch')
+        
+        fireEvent.click(checkbox)
+        expect(browser.tabs.sendMessage).toBeCalledWith(1, {enabled: false, n: true})
+        
+        jest.resetAllMocks()
+
+        fireEvent.click(checkbox)
+        expect(browser.tabs.sendMessage).toBeCalledWith(1, {enabled: false, n: false})
     })
 })
