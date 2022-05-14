3cfe2ae5efde4d5b4ec29a5fa59dfbbae98e09b7
hcortizcolon <hcortizcolon@gmail.com>
2022-05-14 12:02:12 -0400

    improve code coverage

--- test/source/Background/index.test.ts
+++ test/source/Background/index.test.ts
@@ -7,6 +7,7 @@ describe('Background script', () => {
 
     beforeEach(() => {
         querySpy.mockResolvedValueOnce([{id: 2} as Tabs.Tab])
+        window.localStorage.clear()
     })
     
     test('adds tabs onUpdated listener', () => {
@@ -17,10 +18,16 @@ describe('Background script', () => {
         expect(browser.tabs.onActivated.addListener)
     })
     
-    test('calls tabs sendMessage', async () => {
+    test('calls tabs sendMessage with local storage state', async () => {
         window.localStorage.setItem(japanesifyState, JSON.stringify({enabled: false, n: true}))
         await sendMessage()
-
+        
         expect(browser.tabs.sendMessage).toBeCalledWith(2, {enabled: false, n: true})
+    })
+
+    test('calls tabs sendMessage with default state', async () => {
+        await sendMessage()
+
+        expect(browser.tabs.sendMessage).toBeCalledWith(2, {enabled: false, n: false})
     })
 })
