b8045daf858fb3fee8cdfd6902de7fe977342dad
hcortizcolon <hcortizcolon@gmail.com>
2022-08-10 00:11:31 -0400

    start implementing design

--- source/Popup/Popup.tsx
+++ source/Popup/Popup.tsx
@@ -3,7 +3,7 @@ import browser from 'webextension-polyfill';
 import { defaultJapanesifyState, JAPANESIFY_STATE } from '../utils/constants';
 import { JapanesifyState } from '../utils/types';
 import { getCurrentTabId } from '../utils/utils';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col, InputGroup, FormSelect, FormCheck } from 'react-bootstrap';
 
 const Popup: React.FC = () => {
   const [japanesifyState, setJapanesifyState] = React.useState(
@@ -33,88 +33,143 @@ const Popup: React.FC = () => {
 
   return (
     <section id="popup">
-      <h3>Japanesify!</h3>
-      <Button
-        variant="success"
-        data-testid="enable-button"
-        onClick={handleAction('enabled')}
-      >
-        {enabled ? 'disable' : 'enable'}
-      </Button>
-      <br />
-      <label id="ん-switch">ん</label>
-      <input
-        type="checkbox"
-        id="ん-switch"
-        data-testid="ん-switch"
-        checked={n}
-        onChange={handleAction('n')}
-      />
-      <br />
-      <label id="あ-switch">あ</label>
-      <input
-        type="checkbox"
-        id="あ-switch"
-        data-testid="あ-switch"
-        checked={a}
-        onChange={handleAction('a')}
-      />
-      <label id="い-switch">い</label>
-      <input
-        type="checkbox"
-        id="い-switch"
-        data-testid="い-switch"
-        checked={i}
-        onChange={handleAction('i')}
-      />
-      <label id="う-switch">う</label>
-      <input
-        type="checkbox"
-        id="う-switch"
-        data-testid="う-switch"
-        checked={u}
-        onChange={handleAction('u')}
-      />
-      <label id="え-switch">え</label>
-      <input
-        type="checkbox"
-        id="え-switch"
-        data-testid="え-switch"
-        checked={e}
-        onChange={handleAction('e')}
-      />
-      <label id="お-switch">お</label>
-      <input
-        type="checkbox"
-        id="お-switch"
-        data-testid="お-switch"
-        checked={o}
-        onChange={handleAction('o')}
-      />
-      <label id='"-switch'>"</label>
-      <input
-        type="checkbox"
-        id='"-switch'
-        data-testid='"-switch'
-        checked={dak}
-        onChange={handleAction('dak')}
-      />
-      <label id="°-switch">°</label>
-      <input
-        type="checkbox"
-        id="°-switch"
-        data-testid="°-switch"
-        checked={han}
-        onChange={handleAction('han')}
-      />
-      <label id="きゃ-switch">きゃ</label>
-      <input
-        type="checkbox"
-        id="きゃ-switch"
-        data-testid="きゃ-switch"
-        checked={yoon}
-        onChange={handleAction('yoon')}
-      />
+      <div className='jumbotron'>
+        <h3 className='text-center'>Japanesify!</h3>
+      </div>
+      <Container>
+        <Row>
+          <Col>
+            <InputGroup>
+              <FormSelect>
+                <option selected>Hiragana</option>
+                <option value="1" disabled>More soon</option>
+              </FormSelect>
+              <Button
+              variant="success"
+              data-testid="enable-button"
+              onClick={handleAction('enabled')}
+              >
+                {enabled ? 'disable' : 'enable'}
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="ん-switch"
+              data-testid="ん-switch"
+              checked={n}
+              onChange={handleAction('n')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="ん-switch">ん</label>
+          </Col>
+        </Row>
+        <br />
+        <Row xs={3}>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="あ-switch"
+              data-testid="あ-switch"
+              checked={a}
+              onChange={handleAction('a')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="あ-switch">あ</label>
+          </Col>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="い-switch"
+              data-testid="い-switch"
+              checked={i}
+              onChange={handleAction('i')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="い-switch">い</label>
+          </Col>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="う-switch"
+              data-testid="う-switch"
+              checked={u}
+              onChange={handleAction('u')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="う-switch">う</label>
+          </Col>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="え-switch"
+              data-testid="え-switch"
+              checked={e}
+              onChange={handleAction('e')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="え-switch">え</label>
+          </Col>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="お-switch"
+              data-testid="お-switch"
+              checked={o}
+              onChange={handleAction('o')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="お-switch">お</label>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id='"-switch'
+              data-testid='"-switch'
+              checked={dak}
+              onChange={handleAction('dak')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id='"-switch'>"</label>
+          </Col>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="°-switch"
+              data-testid="°-switch"
+              checked={han}
+              onChange={handleAction('han')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="°-switch">°</label>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col className='d-flex align-items-center'>
+            <FormCheck 
+              type="switch" 
+              id="きゃ-switch"
+              data-testid="きゃ-switch"
+              checked={yoon}
+              onChange={handleAction('yoon')}
+            >
+            </FormCheck>
+            <label className="h2 mb-0" id="きゃ-switch">きゃ</label>
+          </Col>
+        </Row>
+      </Container>
+      <div className="website">
+        <a style={{fontSize: '12px'}} href="https://thorbjxrn.github.io">https://thorbjxrn.github.io
+        </a>
+      </div>
     </section>
   );
 };
